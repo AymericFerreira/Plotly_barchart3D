a03aedff02b7adbadc0e8fec3d4c37594d5ddfa5
Aymeric <aymeric.ferreira@gmail.com>
2020-06-16 11:40:06 -0400

    Create barchat py
    Initial commit of the function

---
+++ barchart.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import numpy as np
+
+
+def plotly_barcharts_3d(x_df, y_df, z_df, x_min=0, y_min=0, z_min='auto', step=1, color='x',
+                        x_legend='auto', y_legend='auto', z_legend='auto', flat_shading=True,
+                        x_title='', y_title='', z_title='', *title):
+    """
+    Convert a dataframe in 3D barcharts similar to matplotlib ones
+        Example :
+        x_df = pd.Series([1, 1, 10, 10])
+        y_df = pd.Series([2, 4, 2 ,4])
+        z_df = pd.Series([10, 30, 20, 45])
+
+    :param x_df: Serie of data corresponding to x axis
+    :param y_df: Serie of data corresponding to y axis
+    :param z_df: Serie of data corresponding to height of the bar chart
+    :param x_min: Starting position for x axis
+    :param y_min: Starting position for y axis
+    :param z_min: Minimum value of the barchart, if set to auto minimum value is 0.8 * minimum of z_df to obtain more
+    packed charts
+    :param step: Distance between two barcharts
+    :param color: Axis to create color, possible parameters are
+    x for a different color for each change of x
+    y for a different color for each change of y
+    or x+y to get a different color for each bar
+    :param x_legend: Legend of x axis, if set to auto the legend is based on x_df
+    :param y_legend: Legend of y axis, if set to auto the legend is based on y_df
+    :param z_legend: Legend of z axis, if set to auto the legend is not shown
+    :param flat_shading:
+    :param x_title: Title of x axis
+    :param y_title: Title of y axis
+    :param z_title: Title of z axis
+    :return: 3D mesh figure acting as 3D barcharts
+    """
+
+    if z_min == 'auto':
+        z_min = 0.8 * min(z_df)
+
+    mesh_list = []
+    colors = px.colors.qualitative.Plotly
+
+    for idx, x_data in enumerate(x_df.unique()):
+        idx2 = 0
+        if color == 'x':
+            colorValue = colors[idx]
+        elif color == 'x+y':
+            colorValue = colors[idx + idx2]
+
+        for idx2, y_data in enumerate(y_df.unique()):
+            if color == 'x+y':
+                colorValue = colors[idx + idx2]
+
+            elif color == 'y':
+                colorValue = colors[idx2]
+            x_max = x_min + step
+            y_max = y_min + step
+
+            z_max = z_df[idx + idx2]
+
+            mesh_list.append(
+                go.Mesh3d(
+                    x=[x_min, x_min, x_max, x_max, x_min, x_min, x_max, x_max],
+                    y=[y_min, y_max, y_max, y_min, y_min, y_max, y_max, y_min],
+                    z=[z_min, z_min, z_min, z_min, z_max, z_max, z_max, z_max],
+                    color=colorValue,
+                    i=[7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+                    j=[3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+                    k=[0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+                    opacity=1,
+                    flatshading=flat_shading,
+                ))
+
+            x_min += 2 * step
+        y_min += 2 * step
+        x_min = 0
+    y_min = 0
+
+    if x_legend == 'auto':
+        x_legend = x_df.unique()
+        x_legend = [str(x_ax) for x_ax in x_legend]
+    if y_legend == 'auto':
+        y_legend = y_df.unique()
+        y_legend = [str(y_ax) for y_ax in y_legend]
+    if z_legend == 'auto':
+        z_legend = None
+
+    fig = go.Figure(mesh_list)
+
+    if z_legend is None:
+        fig.update_layout(scene=dict(
+            xaxis=dict(
+                tickmode='array',
+                ticktext=x_legend,
+                tickvals=np.arange(x_min, len(x_df.unique()) * 2, step=2),
+                title=x_title),
+            yaxis=dict(
+                tickmode='array',
+                ticktext=y_legend,
+                tickvals=np.arange(y_min, len(y_df.unique()) * 2, step=2),
+                title=y_title)))
+
+    else:
+        fig.update_layout(scene=dict(
+            xaxis=dict(
+                tickmode='array',
+                ticktext=x_legend,
+                tickvals=np.arange(x_min, len(x_df.unique()) * 2, step=2),
+                title=x_title),
+            yaxis=dict(
+                tickmode='array',
+                ticktext=y_legend,
+                tickvals=np.arange(y_min, len(y_df.unique()) * 2, step=2),
+                title=y_title)),
+            zaxis=dict(
+                tickmode='array',
+                ticktext=z_legend,
+                tickvals='',
+                title=z_title))
+
+    return fig
