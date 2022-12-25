be65390235b1290b568bd7017c3ba5630ac618cb
AymericFerreira <aymeric.ferreira.1@ulaval.ca>
2022-12-24 19:33:07 -0500

    !feat improve version of CI

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Plotly doesn't provide a simple way to create 3D barcharts as matplotlib does.
 Here is a function to create them easily via Mesh3D, inspired by *empet* post (https://community.plotly.com/t/how-to-do-a-3d-bar-chart-if-possible/32287/2)
 
-Some examples : 
+Some examples :
 
 Minimal example
 
--- test_barchart.py
+++ test_barchart.py
@@ -1,14 +1,20 @@
+from __future__ import annotations
+
 import pandas as pd
 
 from barchart import plotly_barcharts_3d
+
 
 def test_array_from_barchart():
     features = [2, 3, 5, 10, 20]
     neighbours = [31, 24, 10, 28, 48]
     accuracies = [0.9727, 0.9994, 0.9994, 0.9995, 0.9995]
-    fig = plotly_barcharts_3d(features, neighbours, accuracies,
-                        x_title="Features", y_title="Neighbours", z_title="Accuracy").__str__()
-    assert(fig.__str__() == """Figure({
+    fig = plotly_barcharts_3d(
+        features, neighbours, accuracies,
+        x_title='Features', y_title='Neighbours', z_title='Accuracy',
+    ).__str__()
+    assert (
+        fig.__str__() == """Figure({
     'data': [{'color': '#636EFA',
               'flatshading': True,
               'hoverinfo': 'z',
@@ -344,23 +350,30 @@ def test_array_from_barchart():
                                    'title': {'text': 'Neighbours'}},
                          'zaxis': {'tickmode': 'array', 'title': {'text': 'Accuracy'}}},
                'template': '...'}
-})""")
+})"""
+    )
 
     features2 = pd.Series([2, 3, 5, 10, 20])
     neighbours2 = pd.Series([31, 24, 10, 28, 48])
     accuracies2 = pd.Series([0.9727, 0.9994, 0.9994, 0.9995, 0.9995])
-    fig2 = plotly_barcharts_3d(features2, neighbours2, accuracies2,
-                              x_title="Features", y_title="Neighbours", z_title="Accuracy").__str__()
+    fig2 = plotly_barcharts_3d(
+        features2, neighbours2, accuracies2,
+        x_title='Features', y_title='Neighbours', z_title='Accuracy',
+    ).__str__()
 
-    assert(fig.__str__() == fig2.__str__())
+    assert (fig.__str__() == fig2.__str__())
+
 
 def test_array_from_barchart_not_unique():
     features = [2, 2, 4, 4, 20]
     neighbours = [31, 24, 31, 28, 24]
     accuracies = [0.9727, 0.9994, 0.9994, 0.9995, 0.9995]
-    fig = plotly_barcharts_3d(features, neighbours, accuracies,
-                              x_title="Features", y_title="Neighbours", z_title="Accuracy")
-    assert(fig.__str__() == """Figure({
+    fig = plotly_barcharts_3d(
+        features, neighbours, accuracies,
+        x_title='Features', y_title='Neighbours', z_title='Accuracy',
+    )
+    assert (
+        fig.__str__() == """Figure({
     'data': [{'color': '#636EFA',
               'flatshading': True,
               'hoverinfo': 'z',
@@ -696,15 +709,19 @@ def test_array_from_barchart_not_unique():
                                    'title': {'text': 'Neighbours'}},
                          'zaxis': {'tickmode': 'array', 'title': {'text': 'Accuracy'}}},
                'template': '...'}
-})""")
+})"""
+    )
 
     features2 = pd.Series([2, 2, 4, 4, 20])
     neighbours2 = pd.Series([31, 24, 31, 28, 24])
     accuracies2 = pd.Series([0.9727, 0.9994, 0.9994, 0.9995, 0.9995])
-    fig2 = plotly_barcharts_3d(features2, neighbours2, accuracies2,
-                               x_title="Features", y_title="Neighbours", z_title="Accuracy").__str__()
+    fig2 = plotly_barcharts_3d(
+        features2, neighbours2, accuracies2,
+        x_title='Features', y_title='Neighbours', z_title='Accuracy',
+    ).__str__()
 
     assert (fig.__str__() == fig2.__str__())
+
 
 def test_sparse_from_barchart():
     xdf = [1, 10]
@@ -712,7 +729,8 @@ def test_sparse_from_barchart():
     zdf = [10, 30, 20, 45]
     fig = plotly_barcharts_3d(xdf, ydf, zdf, color='x+y')
 
-    assert (fig.__str__() == """Figure({
+    assert (
+        fig.__str__() == """Figure({
     'data': [{'color': '#636EFA',
               'flatshading': True,
               'hoverinfo': 'z',
@@ -771,7 +789,8 @@ def test_sparse_from_barchart():
                                    'title': {'text': ''}},
                          'zaxis': {'tickmode': 'array', 'title': {'text': ''}}},
                'template': '...'}
-})""")
+})"""
+    )
 
     xdf2 = pd.Series([1, 10])
     ydf2 = pd.Series([2, 4])
@@ -786,25 +805,9 @@ def test_sparse_from_barchart_not_unique():
     ydf = [2, 2]
     zdf = [10, 30, 20, 20]
     fig = plotly_barcharts_3d(xdf, ydf, zdf, color='x+y')
-
-    assert (fig.__str__() == """Figure({
-    'data': [{'color': '#636EFA',
-              'flatshading': True,
-              'hoverinfo': 'z',
-              'hovertext': 'text',
-              'i': [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
-              'j': [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
-              'k': [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
-              'opacity': 1,
-              'type': 'mesh3d',
-              'x': [0, 0, 1, 1, 0, 0, 1, 1],
-              'y': [0, 1, 1, 0, 0, 1, 1, 0],
-              'z': [8.0, 8.0, 8.0, 8.0, 10, 10, 10, 10]}],
-    'layout': {'scene': {'xaxis': {'tickmode': 'array', 'ticktext': ['1'], 'tickvals': array([0]), 'title': {'text': ''}},
-                         'yaxis': {'tickmode': 'array', 'ticktext': ['2'], 'tickvals': array([0]), 'title': {'text': ''}},
-                         'zaxis': {'tickmode': 'array', 'title': {'text': ''}}},
-               'template': '...'}
-})""")
+    with open('tests/truth_sparse_from_barchart_not_unique.txt') as f:
+        truth = f.read()
+    assert (fig.__str__() == truth)
 
     xdf2 = pd.Series([1, 1])
     ydf2 = pd.Series([2, 2])
